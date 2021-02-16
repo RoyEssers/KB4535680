c6c0cd1c685fa917d1cb4b80d342223f98409873
Roy Essers <78878162+RoyEssers@users.noreply.github.com>
2021-02-16 10:20:37 +0100

    Add files via upload

---
+++ Get-BitlockerIssues.ps1
@@ -0,0 +1,4 @@
+﻿#https://docs.microsoft.com/en-us/windows-hardware/test/hlk/testref/954cf796-a640-4134-b742-eaf0ed2663ff
+#script to readbitlocker eventlog and return back secure boot issues, which result in PDR Binding Not Possible
+Get-WinEvent -FilterHashTable @{ LogName = 'Microsoft-Windows-BitLocker/BitLocker Management';StartTime=$(Get-Date).AddDays(-1)} -ErrorAction SilentlyContinue | 
+    Where-Object { ($_.Message -like '*BitLocker cannot use Secure Boot for integrity*') -or  ($_.Id -eq 770) } | select -Unique -ExpandProperty Message
---
+++ Test-PCR7Binding.ps1
@@ -0,0 +1,5 @@
+﻿$binding = $False
+Start-Process msinfo32.exe -ArgumentList @('/report',"$env:temp\msinfo.txt") -Wait
+if (select-string -Path "$env:temp\msinfo.txt" -Pattern "Binding Not Possible"){$binding = $True}else{$binding = $False}
+Get-Item "$env:temp\msinfo.txt" | Remove-Item -Force | Out-Null
+return $binding
